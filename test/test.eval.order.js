7a49411dac1a6e39093cb2ce26bdb63fa3d86379
liuhua05 <liuhua05@meituan.com>
2016-04-12 11:47:08 +0800

    fix the evaluation order

--- test/test.eval.order.js
+++ test/test.eval.order.js
@@ -1,9 +1,10 @@
 var assert = require("chai").assert;
 var sinon = require("sinon");
 var co = require("co");
+var _ = require('lodash');
 var _hbs = require("../");
 
-suite("generator helper nesting", function() {
+suite("generator helper evaluation order", function() {
     test("evaluation order of nested helpers", function (done) {
         this.timeout(1000);
         var job = sinon.spy();
@@ -105,7 +106,7 @@ suite("generator helper nesting", function() {
     });
 
     test('evaluation order of nested generator helpers', function (done) {
-        this.timeout(2000);
+        this.timeout(1000);
         var job = sinon.spy();
         var hbs = _hbs.create();
 
@@ -126,9 +127,15 @@ suite("generator helper nesting", function() {
 
                 var subres = "";
 
-                // prevent infinite loop
                 if (options.fn) {
-                    subres = options.fn(this);
+                    var scope = {};
+                    if (options.data) {
+                        scope = _.assignIn(scope, options.data);
+                        if (scope.root && '__nextLayer__' in scope.root) {
+                            scope.root = scope.root['__nextLayer__'];
+                        }
+                    }
+                    subres = options.fn(Object.create(this), {data: scope});
                 }
 
                 yield next;
@@ -145,7 +152,60 @@ suite("generator helper nesting", function() {
             var output = yield *cache(data);
             assert(job.called);
             assert.equal(output.toString(), "1.AB2.B");
-            assert.equal(evalOrder.toString(), "pre-1,pre-2,pre-1.1,post-1.1,post-2,post-1");
+            assert.equal(evalOrder.toString(), "pre-1,pre-1.1,pre-2,post-2,post-1.1,post-1");
+            done();
+        });
+    });
+
+    test('evaluation order of nested generator helpers with `each` helper', function (done) {
+        this.timeout(2000);
+        var job = sinon.spy();
+        var hbs = _hbs.create();
+
+        var mockyB = "B";
+        var mockyA = "A";
+        var array = ["1.1", "1.2", "1.3"];
+
+        var template = '1.{{#gn mockyA order=1.0}}{{#each array}}{{gn "C" order=this}}{{/each}}{{/gn}}2.{{gn mockyB order=2.0}}';
+        var data = { mockyB: mockyB, mockyA: mockyA, array: array };
+
+        var evalOrder = [];
+
+        hbs.registerGeneratorHelper("gn", function(name, options) {
+            return function*(next) {
+                job.call();
+                var resA = yield Promise.resolve(name);
+
+                evalOrder.push("pre-" + options.hash.order);
+
+                var subres = "";
+
+                if (options.fn) {
+                    var scope = {};
+                    if (options.data) {
+                        scope = _.assignIn(scope, options.data);
+                        if (scope.root && '__nextLayer__' in scope.root) {
+                            scope.root = scope.root['__nextLayer__'];
+                        }
+                    }
+                    subres = options.fn(Object.create(this), {data: scope});
+                }
+
+                yield next;
+
+                evalOrder.push("post-" + options.hash.order);
+
+                return resA + subres;
+            };
+        });
+
+        var cache = hbs.render(template);
+
+        co(function*(){
+            var output = yield *cache(data);
+            assert(job.called);
+            assert.equal(output.toString(), "1.ACCC2.B");
+            assert.equal(evalOrder.toString(), "pre-1,pre-1.1,pre-1.2,pre-1.3,pre-2,post-2,post-1.3,post-1.2,post-1.1,post-1");
             done();
         });
     });
